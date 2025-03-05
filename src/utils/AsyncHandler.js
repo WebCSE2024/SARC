f0e805781d061fa2f38d62d448bfa7f3cab3c6b7
Jyoti Prakash Panda <147559530+jytpr5221@users.noreply.github.com>
2025-03-06 01:30:12 +0530

    utils added

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,12 @@
+export const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+      try {
+        await requestHandler(req, res, next);
+      } catch (err) {
+        next(err);
+      }
+    };
+  };
+  
+   
+  
