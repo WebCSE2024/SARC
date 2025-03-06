372c9f5d4ce5539c47b18a69673fa25f90506768
Jyoti Prakash Panda <147559530+jytpr5221@users.noreply.github.com>
2025-03-06 14:37:57 +0530

    modified

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-import express, { json, urlencoded } from 'express'
 import cors from 'cors'
-import { errorHandler } from './src/middlewares/errorhandler';
+import { errorHandler } from './src/middlewares/errorhandler.js';
+import  {setUser}  from './src/middlewares/setUser.js';
+import referralRouter from './src/routes/referral.routes.js'
+import express from 'express'
 
 const app= express()
-
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.get('/',(req,res)=>{
+    res.send('hello from server')
+})
 //api routes
-
+app.get('/api',(req,res)=>{
+    res.send('hello from server api')
+})
+app.use('/api/referral',referralRouter)
 
 app.use(errorHandler)
 export {app}
---
+++ src/randomdata/prof.random.js
@@ -0,0 +1,123 @@
+export const profs = [
+    {
+      "full_name": "Minerva Shattock",
+      "linkedIn": "https://usatoday.com/eget.html?nam=nec&congue=nisi...",
+      "email": "mshattock0@shop-pro.jp",
+      "password": "$2a$04$x1CzLAnMbjVpupPFwHZGouIkZsXGWavleYMlG6xBU8Rq2GI22DJv.",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Michael Ebbett",
+      "linkedIn": "http://va.gov/congue/elementum/in/hac.jpg?porttitor=turpis...",
+      "email": "mebbett1@icq.com",
+      "password": "$2a$04$zI8b8eIwenAaK27X6R9P7OC6SswykIc0WCIEPSv3yeNhPcTQMDSLy",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Tania Crone",
+      "linkedIn": "http://nasa.gov/vehicula.jsp?elementum=ipsum&ligula=aliquam...",
+      "email": "tcrone2@drupal.org",
+      "password": "$2a$04$kmHSSt1m.LxidhFiLBHnEeYC95iFOc.n5YLjEVgx7DnOf80SraHz6",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Dick Creddon",
+      "linkedIn": "https://infoseek.co.jp/neque/vestibulum/eget/vulputate/...",
+      "email": "dcreddon3@jalbum.net",
+      "password": "$2a$04$.ctPZXFCch4tFgfggGxuDu4XufoQuW9EaBG6QUsNvOxkVVs4pHSg2",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Josepha Hawton",
+      "linkedIn": "https://simplemachines.org/a/feugiat/et/eros/vestibulum...",
+      "email": "jhawton4@taobao.com",
+      "password": "$2a$04$hezygL0jOlo/fE7MOGbd0uAkQLNS3a80r7AYM8ujLxO204P8.Mwoa",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Robertson Dulson",
+      "linkedIn": "http://mashable.com/dui/luctus/rutrum/nulla/tellus/in/sagittis...",
+      "email": "rdulson5@google.com.hk",
+      "password": "$2a$04$LgEzCJL52QXkQHwXXgzZje/T1XtM3Z6lvMZJ5OMcWSHZfYUN2HfKO",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Tore Greet",
+      "linkedIn": "https://list-manage.com/elit/ac/nulla/sed.aspx?eu=tincidunt...",
+      "email": "tgreet6@comsenz.com",
+      "password": "$2a$04$ip6IxDkEfXsqyCEEErjEOu4Qu8UMQuGaAGzptZmR/N0MfBFCVBRqC",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Jasmina Gravenor",
+      "linkedIn": "https://dagondesign.com/ligula/in/lacus/curabitur/at.aspx...",
+      "email": "jgravenor7@wordpress.com",
+      "password": "$2a$04$UsN2rN39kczNkPiM3YiMeOC3rw.9n3AZuPQF3a6RdrDO3DNOk5Qv6",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Giorgi Scard",
+      "linkedIn": "https://sciencedaily.com/in/quis/justo/maecenas.aspx...",
+      "email": "gscard8@bbb.org",
+      "password": "$2a$04$uAyePbXeEuHyF7QOgqj9ieLeH6jfrYib.5p5ekjVXTXs4ejHCjTKu",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Claribel Lynskey",
+      "linkedIn": "https://state.gov/purus/phasellus/in/felis/donec/semper.aspx...",
+      "email": "clynskey9@oracle.com",
+      "password": "$2a$04$ML8YWB8SoXUs0cO6s7wZVO1YOZVaKMU2H1hRH59ihv6PgBMS8LWx2",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Haskell Smissen",
+      "linkedIn": "https://about.com/amet/eleifend/pede.aspx...",
+      "email": "hsmissena@uol.com.br",
+      "password": "$2a$04$NdVKiNBsBQO1GG7VHd3ws.TmlRvc8lRxAmDKIdow5E3/f.pK8LPZm",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Linette Heathwood",
+      "linkedIn": "https://deviantart.com/morbi/sem/mauris.json...",
+      "email": "lheathwoodb@noaa.gov",
+      "password": "$2a$04$KvVZcX7V7Yx6zWWL1KZ5JuXTxj79GxPKxvJS7oLFAmVxyH/ULlhTC",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Cassandra Biddiss",
+      "linkedIn": "http://google.com.au/orci/luctus/et/ultrices.html...",
+      "email": "cbiddissc@naver.com",
+      "password": "$2a$04$WT7XLO59YZ9lW/f6p2R1V.O/wXYUyM29BHeusQ1YI5B3mYdtbPt0S",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Ellynn Mansell",
+      "linkedIn": "http://comcast.net/mattis/pulvinar.json...",
+      "email": "emanselld@linkedin.com",
+      "password": "$2a$04$sjFQd4oYXpiHcQ/JNaP1Qur4Xx5XZjv2tv2lQzGA9SH4MS8y7lnzS",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    },
+    {
+      "full_name": "Julius Dowding",
+      "linkedIn": "http://imgur.com/pede.json...",
+      "email": "jdowdinge@tinyurl.com",
+      "password": "$2a$04$PpFBW3YZ84TmWAd7f0ZxGe6a4hzY1W3U0G9NHEpR5x3cdX5kPdeFu",
+      "isVerified": true,
+      "role": "PROFESSOR"
+    }
+  ]
+  
