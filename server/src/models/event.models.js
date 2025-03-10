2d9d918e4fcdbed443a59aba29d69e312a64931c
Jyoti Prakash Panda <147559530+jytpr5221@users.noreply.github.com>
2025-03-10 16:05:36 +0530

    install  packages and modified  models

--- server/src/models/event.models.js
+++ server/src/models/event.models.js
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose';
 
 const EventSchema = new mongoose.Schema({
-  
+  eventId:{
+    type:String,
+    unique:true,
+    required:true
+  },
   title: {
     type: String,
     required: true
   },
   eventDate: {
-    type: Date,
-    required: true
+    type:Date,
+    required:[true,"Event-date is needed"],
+    validate:{
+        validator:(value)=> !isNaN(new Date(value).getTime()),
+        message:"Invalid date format"
+    },
   },
   reg_url: {
     type: String
